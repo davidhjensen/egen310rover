ac7b02fd0f2e56505b1f908d32e3c75e40d4590e
David Jensen <dhjensen02@gmail.com>
2024-03-19 08:09:13 -0600

    debugged temp/humidity test

--- temp_humid_test.py
+++ temp_humid_test.py
@@ -3,4 +3,4 @@ import adafruit_ahtx0
 
 sensor = adafruit_ahtx0.AHTx0(board.I2C())
 
-print("Temperature: %.2f degC\nHumidity: %.2f" % (sensor.temperature, sensor.relative_humidity))
+print("Temperature: %.2f degC\nHumidity: %.2f%%" % (sensor.temperature, sensor.relative_humidity))
